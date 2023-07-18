2b73b2c9b59817e5050738410a93383402fcbd11
Aditya Chaturvedi <79988022+adityachaturvedii@users.noreply.github.com>
2023-07-19 00:00:59 +0530

    Add files via upload
    Initial Commit

---
+++ file.py
@@ -0,0 +1,14 @@
+def StringChallenge(str):
+    for i in range(len(str)):
+        digit = int(str[i])
+        remaining_length = len(str) - i
+        if remaining_length < digit:
+            continue
+        substring = str[i:i+digit]
+        if substring == str[i] * digit:
+            return "true"
+    return "false"
+
+input_str ="564244342544443"
+result = StringChallenge(input_str)
+print(result)
